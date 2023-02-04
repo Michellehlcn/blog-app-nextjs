9310782173051ccafd4857d89f1cc50cc68b97db
Michellehlcn <michellehlcn.au@gmail.com>
2023-02-04 15:30:21 +1100

    fix homepage

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -16,8 +16,8 @@ const Main = () => {
           }}>
             <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
                 <div>
-                    <p className="uppercase text-sm tracking-wildest text-gray-600">A software Developer/ freelancer based in Sydney. </p>
-                    <h1 className="py-4 ">
+                    <p className="uppercase text-sm tracking-wildest text-gray-600 pb-3">A software Developer/ freelancer based in Sydney. </p>
+                    <a className="py-3 text-7xl ">
                         <span className="text-[#38bdf8]"> 
                              <Typical
 									loop={Infinity}
@@ -26,23 +26,27 @@ const Main = () => {
 										1000,
 										]}/>
                         </span>
-                    </h1>
-                    <p className="py-4 text-gray-600 max-w-[70%] m-auto">Hello, My name's Michelle. I'm currently working on projects domain in Back-end development, Data Analyst. See more about me.</p>
+                    </a>
+                    <p className="py-5 pb-10 text-gray-600 max-w-[70%] mx-auto font-thin">
+                        Hello, My name's Michelle. I'm currently working on projects domain in Back-end development, Data Analyst.
+                    </p>
+                    <a className="border-slate-50 border-2 rounded-lg py-3 p-3 ring-2 cursor-pointer mx-auto text-[#83a2b4] hover:text-[#38bdf8]
+                        ">See more about me.</a>
                     
-                    <div className=" py-4 px-40 flex items-center justify-center mx-w-[330px] m-auto">
-                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <div className=" py-10 px-40 flex items-center justify-center mx-w-[330px] m-auto">
+                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#38bdf8] hover:shadow-[#38bdf8]">
                             <FaLinkedinIn />
                         </div>
-                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#38bdf8 hover:shadow-[#38bdf8]">
                             <FaGithub />
                         </div>
-                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#38bdf8] hover:shadow-[#38bdf8]">
                             <AiOutlineMail />
                         </div>
-                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#38bdf8] hover:shadow-[#38bdf8]">
                             <FaTwitter />
                         </div>
-                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                        <div className="mx-2 rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#38bdf8] hover:shadow-[#38bdf8]">
                             <BsFillPersonLinesFill />
                         </div>
                     </div>
