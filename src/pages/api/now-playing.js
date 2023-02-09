76e389b81632304f381d8c66c2afddd9ffade1b6
Michellehlcn <michellehlcn.au@gmail.com>
2023-02-09 22:33:31 +1100

    move key to local env

--- src/pages/api/now-playing.js
+++ src/pages/api/now-playing.js
@@ -1,21 +1,32 @@
-var SPOTIFY_REFRESH_TOKEN="";
-var SPOTTIFY_CLIENT_ID="6d34de28c3e4e909045a4bc89f7715a";
-var SPOTIFY_CLIENT_SECRET="a7d3ea3682464d3a80f1270dc3e68ae5";
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+import { env } from 'process';
+import querystring from 'querystring';
 
+var SPOTIFY_REFRESH_TOKEN= process.env.SPOTIFY_REFRESH_TOKEN;
+var SPOTTIFY_CLIENT_ID= process.env.SPOTTIFY_CLIENT_ID;
+var SPOTIFY_CLIENT_SECRET= process.env.SPOTIFY_CLIENT_SECRET;
+var basic = Buffer.from(`${SPOTTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
+
+console.log(SPOTIFY_CLIENT_SECRET, "|",SPOTIFY_REFRESH_TOKEN,"|", basic);
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+var scope = 'playlist-read-private user-read-currently-playing' ;
+var REDIRECT_URI = 'http://localhost:3000/';
 
 const getAccessToken = async () =>{
-    const refresh_token = SPOTIFY_REFRESH_TOKEN;
-    const response = await fetch ("https://accounts.spotify.com/api/token", {
+    const response = await fetch (TOKEN_ENDPOINT, {
         method: "POST",
         headers: {
-            Authorization: `Basic ${Buffer.from(`${SPOTTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString("base64")}`,
+            Authorization: `Basic ${basic}`,
             "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: new URLSearchParams({
+        body: querystring.stringify({
             grant_type: "refresh_token",
-            refresh_token,
-        }),
-    });
+            refresh_token: SPOTIFY_REFRESH_TOKEN,
+        })
+    })
+    console.log("[AccessToken]",response.status)
     return response.json();
 };
 
@@ -31,21 +42,23 @@ const currentlyPlayingSong = async () =>{
 export default async function handler(req, res) {
     
     const response = await currentlyPlayingSong();
+    console.log(response);
 
     if (response.status === 204 || response.status > 400) {
-        console.log(response.status);
         return res.status(200).json({ isPlaying: false });
     }
 
     const song = await response.json();
+    console.log(song);
     
     /* Extract info */
     const isPlaying = song.is_playing;
     const title = song.item.name;
     const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
     const album = song.item.album.name;
-    const albumImageUrl = song.item.album.image[0].url;
-    const songUrl = song.item.external_urls.sportify;
+    const albumImageUrl = song.item.album.images[0].url;
+    console.log(JSON.stringify(albumImageUrl));
+    const songUrl = song.item.external_urls.spotify;
 
     /* Return the data as JSON */
     return res.status(200).json({
